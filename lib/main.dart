6eb5c323a93b3daf85f9a6eb3603321dd78faf71
denis_levchenko <tranzi90@gmail.com>
2024-03-10 21:56:05 +0300

    add dashatar asset

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Color(0xff000000),
         appBar: AppBar(
           backgroundColor: Color(0xff000000),
           centerTitle: true,
           title: Text("My Dashatar App"),
+        ),
+        body: Center(
+          child: Image.asset('images/dashatar.png'),
         ),
       ),
     ),
